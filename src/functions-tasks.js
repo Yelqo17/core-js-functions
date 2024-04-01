e8afa0286f1e3ca1fde85071ae837365f90e4370
Andrey Kozimirov <an.kozimirov@gmail.com>
2024-04-01 17:02:33 +0300

    feat: implement getPowerFunction function

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -77,8 +77,11 @@ function getArgumentsCount(funcs) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  const closure = function clos(num) {
+    return num ** exponent;
+  };
+  return closure;
 }
 
 /**
