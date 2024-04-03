0f43de6b8afa6fde555b270084d86876eb2090dc
Andrey Kozimirov <an.kozimirov@gmail.com>
2024-04-03 14:53:20 +0300

    feat: implement partialUsingArguments fucntion

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -211,8 +211,11 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  const res = function resultFunc(...args2) {
+    return fn(...args1, ...args2);
+  };
+  return res;
 }
 
 /**
